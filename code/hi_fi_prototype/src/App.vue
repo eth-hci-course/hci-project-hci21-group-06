7f3256cba996b1fb363926493b87acd62ed6dc24
softestsoph <43212194+softestsoph@users.noreply.github.com>
2021-11-08 21:22:50 +0100

    format

--- code/hi_fi_prototype/src/App.vue
+++ code/hi_fi_prototype/src/App.vue
@@ -9,7 +9,7 @@
         <md-bottom-bar md-type="shift" md-sync-route>
           <md-bottom-bar-item id="bottom-bar-item-Dashboard" md-label="Dashboard" md-icon="dashboard" to='/dashboard'></md-bottom-bar-item>
           <md-bottom-bar-item id="bottom-bar-item-Rankings" md-label="Rankings" md-icon="leaderboard" to='/rankings'></md-bottom-bar-item>
-          <md-bottom-bar-item id="bottom-bar-item-shop" md-label="Shop" md-icon="local_mall" to="/shop"></md-bottom-bar-item>
+          <md-bottom-bar-item id="bottom-bar-item-shop" md-label="Shop" md-icon="local_mall" to='/shop'></md-bottom-bar-item>
           <md-bottom-bar-item id="bottom-bar-item-stats" md-label="Analytics" md-icon="analytics" to='/analytics'></md-bottom-bar-item>
           <md-bottom-bar-item id="bottom-bar-item-settings" md-label="Settings" md-icon="settings" to='/settings'></md-bottom-bar-item>
         </md-bottom-bar>  
