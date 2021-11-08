ab9e0f020f8bb957458c062c03e07b663fb10360
Marco Schoeb <mschoeb@student.ethz.ch>
2021-11-08 16:28:46 +0100

    first test commit

--- code/hi_fi_prototype/src/pages/Dashboard/Home.vue
+++ code/hi_fi_prototype/src/pages/Dashboard/Home.vue
@@ -15,6 +15,20 @@
         <md-button>Action</md-button>
       </md-card-actions>
     </md-card>
+  <md-card class="card">
+    <md-card-header>
+      <div class="md-title">Card</div>
+    </md-card-header>
+
+    <md-card-content>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+    </md-card-content>
+
+    <md-card-actions>
+      <md-button>Action</md-button>
+      <md-button>Action</md-button>
+    </md-card-actions>
+  </md-card>
 </div>
 </template>
 
