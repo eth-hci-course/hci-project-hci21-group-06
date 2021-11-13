75c81acc949264740b5e7caa1a5001b4a4dbb308
Marco Schoeb <mschoeb@student.ethz.ch>
2021-11-13 20:10:27 +0100

    somewhat acceptable version with dialogs

--- code/hi_fi_prototype/src/pages/Dashboard/Home.vue
+++ code/hi_fi_prototype/src/pages/Dashboard/Home.vue
@@ -1,5 +1,7 @@
 <template>
 <div class="dashboard">
+  <md-button class="md-button md-raised" v-on:click="ab=false">List</md-button>
+  <md-button class="md-button md-raised" v-on:click="ab=true">Cards</md-button>
   <br><br>
   <div class="upper">
     <div class="upper-text">you have used</div><br>
@@ -15,40 +17,40 @@
     </div>
   </div>
 <br>
-  <div class="card-wrapper" v-if="ab">
-    <md-card class="card" v-for="item in challenges" :key="item.title">
+  <div class="card-wrapper md-elevation-4" v-if="ab">
+    <md-card class="card md-elevation-4" v-for="item in challenges" :key="item.title">
       <md-card-header>
         <div class="md-title">{{ item.title }}</div>
       </md-card-header>
       <md-card-content>
         {{ item.content }}
       </md-card-content>
-      <md-card-actions>
-        <md-button class="md-icon-button"><md-icon>done</md-icon></md-button>
-        <md-button class="md-icon-button"><md-icon>close</md-icon></md-button>
+      <md-progress-bar v-if="item.accepted" md-mode="determinate" :md-value="50" ></md-progress-bar>
+      <md-card-actions v-else="">
+        <md-button class="md-button" v-on:click="onConfirm(item)">Accept</md-button>
       </md-card-actions>
     </md-card>
   </div>
 
-  <div class="list-wrapper" v-else>
-    <md-list  v-for="item in challenges" :key="item.title">
-      <md-list-item class="listitem">
-        <div class="md-list-item-text" @click="item.show = true">
-          <span>{{ item.title }}</span>
-          <span>{{ item.content }}</span>
-        </div>
-        <md-button class="md-icon-button"><md-icon>done</md-icon></md-button>
-        <md-button class="md-icon-button"><md-icon>close</md-icon></md-button>
-      </md-list-item>
-      <md-divider></md-divider>
-      <div>
-        <md-dialog-confirm
-            :md-active.sync="item.show"
-            :md-title="item.title"
-            :md-content="item.content"
-            md-confirm-text="Accept" >
-        </md-dialog-confirm>
-      </div>
+  <div class="list-wrapper md-elevation-4" v-else>
+    <md-list>
+      <template v-for="(item, index) in challenges">
+        <md-divider v-if="index>0"></md-divider>
+        <md-list-item class="listitem">
+          <div class="md-list-item-text" @click="item.show = true">
+            <span>{{ item.title }}</span>
+            <span>{{ item.content }}</span>
+            <md-progress-bar style="margin-top: 10px;" v-if="item.accepted" md-mode="determinate" :md-value="33"></md-progress-bar>
+          </div>
+          <md-dialog-confirm
+              :md-active.sync="item.show"
+              :md-title="item.title"
+              :md-content="item.content"
+              md-confirm-text="Accept"
+              @md-confirm="onConfirm(item)">
+          </md-dialog-confirm>
+        </md-list-item>
+      </template>
     </md-list>
   </div>
 
@@ -66,13 +68,18 @@ export default {
       coins: 78,
       ab: false,
       challenges: [
-        {title: "Chill out", content: "reduce the energy usage of your fridge over the next 7 days by 30%", img: "", show: false},
-        {title: "Clean dishes", content: "Over the next 30 days, use your dishwasher only once per day", img: "", show: false},
-        {title: "Good cooking", content: "Never use more than two stove plates at once for a week", img: "", show: false},
-        {title: "Not always on", content: "Turn off your Laptop and PC during the night", img: "", show: false}
+        {title: "Chill out", content: "reduce the energy usage of your fridge over the next 7 days by 30%", img: "", show: false, accepted: false},
+        {title: "Clean dishes", content: "Over the next 30 days, use your dishwasher only once per day", img: "", show: false, accepted: true},
+        {title: "Good cooking", content: "Never use more than two stove plates at once for a week", img: "", show: false, accepted: false},
+        {title: "Not always on", content: "Turn off your Laptop and PC during the night", img: "", show: false, accepted: false}
       ]
 
 
+    }
+  },
+  methods: {
+    onConfirm(item) {
+      item.accepted = true
     }
   }
 }
@@ -98,24 +105,19 @@ export default {
 }
 
 .card-wrapper {
-  overflow-x: auto;
-  white-space: nowrap;
-  display: grid;
-  max-width: 100%;
-  grid-template-columns: repeat(4, 1fr);
-  grid-auto-rows: minmax(10px, auto);
-  grid-gap: 1rem;
-  padding: 1rem;
+  overflow: scroll;
+  max-height: 400px;
 }
 .card {
-  white-space: normal;
-  margin: 5px;
-  max-width: 320px;
+  margin: 10px;
 }
 
 .list-wrapper {
   max-width: 100%;
 }
 .listitem {
+  margin: 5px;
+  margin-top: 10px;
+  margin-bottom: 10px;
 }
 </style>
