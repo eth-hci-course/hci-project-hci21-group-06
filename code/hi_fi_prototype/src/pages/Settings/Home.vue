d114adec276f67b9b90733de1873a2bbbf3c96c2
Markus Wagner <wagnemar@student.ethz.ch>
2021-11-15 14:51:44 +0100

    Made Settings nicer

--- code/hi_fi_prototype/src/pages/Settings/Home.vue
+++ code/hi_fi_prototype/src/pages/Settings/Home.vue
@@ -1,23 +1,21 @@
 <template>
   <div>
-
-    <p><font size="7"> Settings </font></p>
-    <br /> 
+    <center><p><font size="7"> Settings </font></p></center>
+    <br />
 
     <md-avatar class="md-avatar-icon md-large md-primary">
      <md-icon>person_outline</md-icon>
     </md-avatar>
 
-    <center><p><font size="5"><b>Jane Doe</b></font></p></center>
+    <center><p><font size="5"><b>Your Name</b></font></p></center>
 
-    <md-switch v-model="boolean_daily">Daily Notification</md-switch>
-    <md-switch v-model="boolean_contacts">Show Contacts</md-switch>
-    <md-switch v-model="boolean_newsletter">Email Newsletter</md-switch>
-    <md-switch v-model="boolean_incognito">Incognito Mode</md-switch>
-    <md-switch v-model="boolean_reserve1">Reserve 1</md-switch>
-    <md-switch v-model="boolean_reserve2" >Reserve 2</md-switch>
+    <p style="text-align:left"><md-switch v-model="boolean_daily">Daily Notification</md-switch></p> <br>
+    <p style="text-align:left"><md-switch v-model="boolean_contacts">Show Contacts</md-switch></p> <br>
+    <p style="text-align:left"><md-switch v-model="boolean_newsletter">Email Newsletter</md-switch></p> <br>
+    <p style="text-align:left"><md-switch v-model="boolean_incognito">Incognito Mode</md-switch></p> <br> 
 
-    <table>
+    <!-- If you want to show what the value of the params is  -->
+    <!-- <table>
       <tr>
         <th>Daily Notification</th>
         <th>Show Contacts</th>
@@ -31,7 +29,15 @@
         <td>{{ boolean_newsletter }}</td>
         <td>{{ boolean_incognito }}</td>
       </tr>
-    </table>
+    </table> -->
+
+      <md-card class="card">
+
+      <md-card-content>
+        <b>Delete account</b> 
+      </md-card-content>
+
+    </md-card>
   </div>
 </template>
 
@@ -44,15 +50,13 @@
       boolean_contacts: true,
       boolean_newsletter: false,
       boolean_incognito: false,
-      boolean_reserve1: true,
-      boolean_reserve2: true
     })
   }
 </script>
 
 <style lang="css" scoped>
   .md-switch {
-    display: flex;
+    margin: auto;
   }
 
   table {
@@ -65,5 +69,14 @@
   display: block;
   margin-left: auto;
   margin-right: auto;
+  }
+
+  .card {
+    width: 80%;
+    background-color: red;
+    color: white;
+    margin: auto;
+    text-align: center;
+    border-radius: 20px;
   }
 </style>
